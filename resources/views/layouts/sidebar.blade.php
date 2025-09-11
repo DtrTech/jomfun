c59825051c22c222360140a1630ca61a6d0921f6
ffenggg <wwfeng1999@gmail.com>
2025-09-11 11:57:34 +0800

    a

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -28,11 +28,63 @@ $currentRoute = request()->route()->getName();
 		<li class="menu {{ request()->routeIs('home') ? 'active' : '' }}">
 			<a href="{{ route('home') }}" aria-expanded="false" class="dropdown-toggle">
 				<div class="">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+					<svg xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 24 24" width="24" height="24" 
+						fill="none" stroke="currentColor" stroke-width="2" 
+						stroke-linecap="round" stroke-linejoin="round" 
+						class="feather feather-home">
 						<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
 						<polyline points="9,22 9,12 15,12 15,22"></polyline>
 					</svg>
 					<span>Home</span>
+				</div>
+			</a>
+		</li>
+
+		<li class="menu {{ request()->routeIs('main') ? 'active' : '' }}">
+			<a href="{{ route('main') }}" aria-expanded="false" class="dropdown-toggle">
+				<div class="">
+					<svg xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 24 24" width="24" height="24" 
+						fill="none" stroke="currentColor" stroke-width="2" 
+						stroke-linecap="round" stroke-linejoin="round" 
+						class="feather feather-home">
+						<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+						<polyline points="9,22 9,12 15,12 15,22"></polyline>
+					</svg>
+					<span>Main Page</span>
+				</div>
+			</a>
+		</li>
+
+		<li class="menu {{ request()->routeIs('category') ? 'active' : '' }}">
+			<a href="{{ route('category') }}" aria-expanded="false" class="dropdown-toggle">
+				<div class="">
+					<svg xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 24 24" width="24" height="24" 
+						fill="none" stroke="currentColor" stroke-width="2" 
+						stroke-linecap="round" stroke-linejoin="round" 
+						class="feather feather-home">
+						<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+						<polyline points="9,22 9,12 15,12 15,22"></polyline>
+					</svg>
+					<span>Category</span>
+				</div>
+			</a>
+		</li>
+
+		<li class="menu {{ request()->routeIs('project') ? 'active' : '' }}">
+			<a href="{{ route('project') }}" aria-expanded="false" class="dropdown-toggle">
+				<div class="">
+					<svg xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 24 24" width="24" height="24" 
+						fill="none" stroke="currentColor" stroke-width="2" 
+						stroke-linecap="round" stroke-linejoin="round" 
+						class="feather feather-home">
+						<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+						<polyline points="9,22 9,12 15,12 15,22"></polyline>
+					</svg>
+					<span>Project</span>
 				</div>
 			</a>
 		</li>
