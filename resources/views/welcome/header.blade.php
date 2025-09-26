d744428411e5e35db6c2355113512c213320e464
ffenggg <wwfeng1999@gmail.com>
2025-09-26 11:44:15 +0800

    a

---
+++ resources/views/welcome/header.blade.php
@@ -0,0 +1,66 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Jomfun</title>
+
+<!-- Favicon -->
+<link rel="icon" href="{{ asset('image/jomfun/J.png') }}" sizes="32x32">
+<link rel="icon" href="{{ asset('image/jomfun/J.png') }}" sizes="192x192">
+<link rel="apple-touch-icon" href="{{ asset('image/jomfun/J.png') }}">
+<meta name="msapplication-TileImage" content="{{ asset('image/jomfun/J.png') }}">
+
+<!-- Essential WordPress CSS -->
+<link rel="stylesheet" href="https://klfoodie.com/wp-includes/css/dist/block-library/style.min.css?ver=6.8.2">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/themes/jannah/assets/css/base.min.css?ver=5.4.5">
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/themes/jannah/assets/css/style.min.css?ver=5.4.5">
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/themes/jannah/assets/css/widgets.min.css?ver=5.4.5">
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/themes/jannah/assets/css/helpers.min.css?ver=5.4.5">
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/themes/jannah/assets/css/fontawesome.css?ver=5.4.5">
+
+<!-- Plugin CSS -->
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=6.1.1">
+<link rel="stylesheet" href="https://klfoodie.com/wp-content/plugins/jetpack/modules/likes/style.css?ver=14.9.1">
+
+<!-- WordPress Meta -->
+<link rel="https://api.w.org/" href="https://klfoodie.com/wp-json/">
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://klfoodie.com/xmlrpc.php?rsd">
+<link rel="shortlink" href="https://wp.me/P9pw18-hCP">
+<meta name="generator" content="WordPress 6.8.2">
+
+<!-- Facebook -->
+<meta property="fb:pages" content="112274292745476">
+
+<!-- Essential JavaScript -->
+<script src="https://klfoodie.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1"></script>
+<script src="https://klfoodie.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"></script>
+
+<!-- Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109829759-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-109829759-1');
+</script>
+
+<!-- Custom Styles -->
+<style>
+    :root {
+        --primary-color: #ffce00;
+        --primary-hover: #cc140f;
+        --text-color: #34495e;
+        --bg-color: #ffffff;
+    }
+    
+    .brand-title, a:hover { color: var(--primary-color); }
+    .button, [type='submit'] { 
+        background-color: var(--primary-color); 
+        color: #ffffff; 
+    }
+    .button:hover, [type='submit']:hover { 
+        background-color: var(--primary-hover); 
+    }
+    
+    .dable-widget { display: none; }
+    .single-post .dable-widget { display: block !important; }
+</style>
