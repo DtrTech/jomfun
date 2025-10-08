dbadfad78aa19c74b273c03b657099b157ee3c21
ffenggg <wwfeng1999@gmail.com>
2025-10-08 14:22:05 +0800

    a

--- resources/views/welcome/layouts/app.blade.php
+++ resources/views/welcome/layouts/app.blade.php
@@ -6,7 +6,7 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     
-    <title>@yield('title', 'WoOx Travel')</title>
+    <title>@yield('title', 'JomFun')</title>
     
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
