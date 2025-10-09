c578d00f0ebdd706acf2a810f0e9435d16b477c0
ffenggg <wwfeng1999@gmail.com>
2025-10-09 14:03:05 +0800

    a

--- resources/views/welcome/description.blade.php
+++ resources/views/welcome/description.blade.php
@@ -61,70 +61,18 @@ body, html {
     </div>
 </div>
 
-<div class="weekly-offers">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 offset-lg-3">
-                <div class="section-heading text-center">
-                    <h2>More from {{ $project->category_name }}</h2>
-                    <p>Discover more trips related to this category below.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="owl-weekly-offers owl-carousel">
-                    @foreach ($relatedProjects as $related)
-                        <div class="item">
-							<div class="thumb">
-								<img src="{{ $related->project_image ? asset($related->project_image) : asset('default-banner.jpg') }}" alt="{{ $related->title }}">
-								<div class="text" style="max-width: 300px; margin: 0 auto;">
-									<h4>
-										<span><i class="fa fa-user"></i> {{ $related->author }}</span>
-									</h4>
-                                    <br>
-                                    <br>
-                                    <span>
-                                        <a href="/category/{{ strtolower($project->category_name) }}" 
-                                            style="text-decoration:none; color:#fff; background:#588800; padding:5px;">
-                                            {{ $project->category_name }}
-                                        </a>
-                                    </span>
-                                    <br>
-									<div class="line-dec"></div>
-									<ul>
-										<li style="white-space: normal; word-wrap: break-word;">
-											{{ \Illuminate\Support\Str::words($related->title, 20, '...') }}
-										</li>
-									</ul>
-									<div class="main-button">
-										<a href="{{ url('/' . \Illuminate\Support\Str::slug(\Illuminate\Support\Str::words($related->title, 10))) }}">Explore More</a>
-									</div>
-								</div>
-							</div>
-						</div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-  <div class="more-about">
+<div class="more-about">
     <div class="container">
         <div class="row">
 			<div class="col-lg-12">
 				<div class="post-meta text-center mb-4">
                     <ul class="list-inline" style="display:flex; justify-content:center; flex-wrap:wrap; gap:15px; padding:0; margin:0;">
                         <li class="list-inline-item mx-3" style="display:flex; align-items:center; gap:8px;">
-                        <img alt="Photo of {{ $project->author }}" 
-                            src="{{ $project->author_image ? asset($project->author_image) : 'https://secure.gravatar.com/avatar/17bbc2d36ab2cd21146949c7e05e9f1d50595287eeeb4c8f37faf69d727634e1?s=140&r=g' }}" 
-                            height="25" width="25" 
-                            style="border-radius:50%; object-fit:cover;">
-                        {{ $project->author }}
+                            <img alt="Photo of {{ $project->author }}" 
+                                src="{{ $project->author_image ? asset($project->author_image) : 'https://secure.gravatar.com/avatar/17bbc2d36ab2cd21146949c7e05e9f1d50595287eeeb4c8f37faf69d727634e1?s=140&r=g' }}" 
+                                height="25" width="25" 
+                                style="border-radius:50%; object-fit:cover;">
+                            {{ $project->author }}
                         </li>
                         <li class="list-inline-item mx-3" style="display:flex; align-items:center; gap:6px;">
                         <i class="fa fa-folder"></i> {{ $project->category_name }}
